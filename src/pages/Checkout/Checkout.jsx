6d8904b2760fc1b378a8ee0d994424cd43f98c11
Ram Goel <rgoel766@gmail.com>
2023-06-13 08:08:55 +0530

    added log of items

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import CheckoutLoader from '../../components/CheckoutLoader/CheckoutLoader'
 import './styles.css'
+import { useSelector } from 'react-redux'
 function Checkout() {
     const [isLoaded, setLoaded] = useState(false)
+    const cart=useSelector(state=>state.app.cart)
     useEffect(e => {
         setTimeout(() => {
             setLoaded(true)
+            console.log(Object.values(cart))
         }, 3000)
     }, [])
     return (
